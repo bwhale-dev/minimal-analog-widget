805e6533bb6e0386ef7c97bb3e1afabaa1750dac
udsukidollcs-dev <ID+username@users.noreply.github.com>
2026-04-01 16:27:08 +0900

    アナログ時計の位置を調整

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -271,7 +271,7 @@ export function Home() {
               <div 
                 className="absolute"
                 style={{
-                  top: isMobile ? '50%' : '56%',  // スマホを48%に調整
+                  top: isMobile ? '45%' : '56%',  // スマホを48%に調整
                   left: '50%', 
                   transform: 'translate(-50%, -50%)',
                   filter: bgColor === '#2d3436' ? 'invert(1) brightness(2)' : 'none',
